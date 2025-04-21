05a2976dcdc58cf9aa004aac3f86a3fd2a42f5ea
Simone Brown <simone.brown19@buc.blinn.edu>
2025-04-20 20:37:56 -0500

    added file

---
+++ php-pages/connection.php
@@ -0,0 +1,22 @@
+<?php
+
+ini_set ('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+$server = "local host";
+$username = "itse1311";
+$password = "itse1311";
+$database = "itse1311";
+
+$conn = new mysqli($server, $username, $password, $database);
+
+//Connection Creation
+/*
+if ($conn->connect_error) {
+    die("Connection failed".$conn->connect_error); 
+}
+
+else {
+    echo "Connection Successful";
+}*/
