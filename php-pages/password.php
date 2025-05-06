8c521dcea8face19e91b875cf80dbea3dec44391
Simone Brown <simone.brown19@buc.blinn.edu>
2025-05-05 20:26:47 -0500

    7B Password Encryption

---
+++ php-pages/password.php
@@ -0,0 +1,32 @@
+<?php
+
+$password [] = "password";
+$password [] = "12345";
+$password [] = "000000";
+$password [] = "itse1311";
+
+$myPassword = "itse1311";
+
+$myPassword = hash('sha256', $myPassword);
+
+
+if (is_array($password)) {
+
+    foreach($password as $passwords) {
+
+        $myHashedPassword = hash('sha256', $passwords);
+
+        if($myHashedPassword == $myPassword) {
+            echo "Got hacked using " . $passwords . " encrypted as " . $myHashedPassword;   
+    }
+    else {
+        echo "No luck using " . $passwords . "<br/>";
+    }
+}
+}
+
+//is array
+
+
+
+//echo $myPassword;
--- php-pages/select.php
+++ php-pages/select.php
@@ -2,26 +2,27 @@
 
 include('connection.php');
 
-$firstName = $_POST['firstName'];
+//$firstName = $_POST['firstName'];
 
-echo $firstName;
+//echo $firstName;
 
 $i = 0;
 
-$query = "SELECT fname FROM myitsetable WHERE fname = 'Simone'ORDER BY fname DESC";
+
+$query = "SELECT firstName FROM contacts WHERE firstName = 'Simone' ORDER BY firstName ASC";
 
 if($stmt=$conn->prepare($query)) {
     /*execute statment*/
     $stmt->execute();
 
     /*stmt bind variables*/ 
-    $stmt->bind_result($fname);
+    $stmt->bind_result($firstName);
 
         /*fetch values*/ 
         while($stmt->fetch()) {
 
             $i++;
-            echo $i . ": " . $fname . "<br />";
+            echo $i . ": " . $firstName . "<br />";
 
         }
 
